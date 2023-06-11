1ba9fe41ba31f44c5e31e73124ffcb892586fe3d
Alexandr Sozonov <sozonovalexander13@gmail.com>
2023-06-12 01:51:54 +0300

    rename AuthConstants

--- app/src/main/java/com/sozonov/gitlabx/auth/AuthConstants.kt
+++ app/src/main/java/com/sozonov/gitlabx/auth/AuthConstants.kt
@@ -1,6 +1,6 @@
 package com.sozonov.gitlabx.auth
 
-class AuthProps {
+class AuthConstants {
     companion object {
         const val CLIENT_ID = "d1292d6764d7aa6a174b29febbd89063e2b32a4ee76e21eae4e8b2b03ecb2fd9"
         const val REDIRECT_URI = "com.sozonov.gitlabx:/oauth2redirect"
--- app/src/main/java/com/sozonov/gitlabx/auth/AuthService.kt
+++ app/src/main/java/com/sozonov/gitlabx/auth/AuthService.kt
@@ -3,9 +3,9 @@ package com.sozonov.gitlabx.auth
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import com.sozonov.gitlabx.auth.AuthProps.Companion.CLIENT_ID
-import com.sozonov.gitlabx.auth.AuthProps.Companion.ENDPOINT
-import com.sozonov.gitlabx.auth.AuthProps.Companion.REDIRECT_URI
+import com.sozonov.gitlabx.auth.AuthConstants.Companion.CLIENT_ID
+import com.sozonov.gitlabx.auth.AuthConstants.Companion.ENDPOINT
+import com.sozonov.gitlabx.auth.AuthConstants.Companion.REDIRECT_URI
 import net.openid.appauth.AuthorizationRequest
 import net.openid.appauth.AuthorizationService
 import net.openid.appauth.AuthorizationServiceConfiguration
