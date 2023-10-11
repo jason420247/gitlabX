52d763f7ca8782840d6d77f589c3db919debbd73
alexandr.sozonov <sozonovalexander13@gmail.com>
2023-10-11 13:32:16 +0300

    feature: simple uncaught exception.

--- app/src/main/java/com/sozonov/gitlabx/GitlabXApplication.kt
+++ app/src/main/java/com/sozonov/gitlabx/GitlabXApplication.kt
@@ -1,6 +1,7 @@
 package com.sozonov.gitlabx
 
 import android.app.Application
+import android.util.Log
 import com.sozonov.gitlabx.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -9,6 +10,10 @@ import org.koin.core.context.startKoin
 class GitlabXApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+            Log.e(thread.name, exception.message, exception)
+        }
 
         startKoin {
             androidLogger()
